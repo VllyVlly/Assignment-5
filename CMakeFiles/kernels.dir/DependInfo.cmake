bae5f4552774cd3c0825948785f5adce24280f57
VllyVlly <virellelly578@gmail.com>
2026-04-20 14:36:58 +0800

    Initial commit

---
+++ CMakeFiles/kernels.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/d/Virelle/Projects/Schoolwork/CSC3060/Assignment 5/CSC3060_Project5_Code_Optimization/src/kernel/bitwise.cpp" "CMakeFiles/kernels.dir/src/kernel/bitwise.cpp.o" "gcc" "CMakeFiles/kernels.dir/src/kernel/bitwise.cpp.o.d"
+  "/mnt/d/Virelle/Projects/Schoolwork/CSC3060/Assignment 5/CSC3060_Project5_Code_Optimization/src/kernel/blackscholes.cpp" "CMakeFiles/kernels.dir/src/kernel/blackscholes.cpp.o" "gcc" "CMakeFiles/kernels.dir/src/kernel/blackscholes.cpp.o.d"
+  "/mnt/d/Virelle/Projects/Schoolwork/CSC3060/Assignment 5/CSC3060_Project5_Code_Optimization/src/kernel/filter_gradient.cpp" "CMakeFiles/kernels.dir/src/kernel/filter_gradient.cpp.o" "gcc" "CMakeFiles/kernels.dir/src/kernel/filter_gradient.cpp.o.d"
+  "/mnt/d/Virelle/Projects/Schoolwork/CSC3060/Assignment 5/CSC3060_Project5_Code_Optimization/src/kernel/graph.cpp" "CMakeFiles/kernels.dir/src/kernel/graph.cpp.o" "gcc" "CMakeFiles/kernels.dir/src/kernel/graph.cpp.o.d"
+  "/mnt/d/Virelle/Projects/Schoolwork/CSC3060/Assignment 5/CSC3060_Project5_Code_Optimization/src/kernel/grff.cpp" "CMakeFiles/kernels.dir/src/kernel/grff.cpp.o" "gcc" "CMakeFiles/kernels.dir/src/kernel/grff.cpp.o.d"
+  "/mnt/d/Virelle/Projects/Schoolwork/CSC3060/Assignment 5/CSC3060_Project5_Code_Optimization/src/kernel/image_proc.cpp" "CMakeFiles/kernels.dir/src/kernel/image_proc.cpp.o" "gcc" "CMakeFiles/kernels.dir/src/kernel/image_proc.cpp.o.d"
+  "/mnt/d/Virelle/Projects/Schoolwork/CSC3060/Assignment 5/CSC3060_Project5_Code_Optimization/src/kernel/matmul.cpp" "CMakeFiles/kernels.dir/src/kernel/matmul.cpp.o" "gcc" "CMakeFiles/kernels.dir/src/kernel/matmul.cpp.o.d"
+  "/mnt/d/Virelle/Projects/Schoolwork/CSC3060/Assignment 5/CSC3060_Project5_Code_Optimization/src/kernel/relu.cpp" "CMakeFiles/kernels.dir/src/kernel/relu.cpp.o" "gcc" "CMakeFiles/kernels.dir/src/kernel/relu.cpp.o.d"
+  "/mnt/d/Virelle/Projects/Schoolwork/CSC3060/Assignment 5/CSC3060_Project5_Code_Optimization/src/kernel/sparse_spmm.cpp" "CMakeFiles/kernels.dir/src/kernel/sparse_spmm.cpp.o" "gcc" "CMakeFiles/kernels.dir/src/kernel/sparse_spmm.cpp.o.d"
+  "/mnt/d/Virelle/Projects/Schoolwork/CSC3060/Assignment 5/CSC3060_Project5_Code_Optimization/src/kernel/trace_replay.cpp" "CMakeFiles/kernels.dir/src/kernel/trace_replay.cpp.o" "gcc" "CMakeFiles/kernels.dir/src/kernel/trace_replay.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
